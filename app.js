e062af52302d8659168885504d85440666a78c06
Akhmad Dani Munif <37365441+gaisuke@users.noreply.github.com>
2020-09-07 21:20:43 +0700

    sidebar toggle

---
+++ app.js
@@ -0,0 +1,11 @@
+function sidebar_close(){
+    document.getElementById("navcloser").style.display = "none";
+    document.getElementById("navopener").style.display = "block";
+    document.getElementById("sidebar").style.display = "none";
+}
+
+function sidebar_open(){
+    document.getElementById("navopener").style.display = "none";
+    document.getElementById("navcloser").style.display = "block";
+    document.getElementById("sidebar").style.display = "block";
+}
